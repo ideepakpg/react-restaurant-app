7f52b1f8901ff073faf701cae3e951df6bf6a695
Deepak PG <deepakpg121@gmail.com>
2023-08-27 13:19:03 +0530

    Export dishes data to dishes page

---
+++ src/StaticData/Data.jsx
@@ -0,0 +1,37 @@
+import Food1 from '../assets/Food1.jpg'
+import Food2 from '../assets/Food2.jpg'
+import Food3 from '../assets/Food3.jpeg'
+
+
+export const Data = [
+    {
+        id: "1",
+        img: Food1,
+        price: "80",
+        item: "Combo Dishes",
+        likes: 2,
+        heart: 2,
+        share: 2000,
+    },
+
+    {
+        id: "2",
+        img: Food2,
+        price: "25",
+        item: "London Popcorn",
+        likes: 3,
+        heart: 2,
+        share: 2300,
+    },
+
+    {
+        id: "3",
+        img: Food3,
+        price: "50",
+        item: "Shawarma Pro",
+        likes: 4,
+        heart: 3,
+        share: 3000,
+    },
+
+];
