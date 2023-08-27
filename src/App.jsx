865f260eb35690df140e9682ad6e5a6bb68a6487
Deepak PG <deepakpg121@gmail.com>
2023-08-27 13:26:06 +0530

    Import dishes, ambience and delivery pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import WelcomePage from './Pages/WelcomePage/WelcomePage';
 import Footer from './Components/Footer/Footer';
 import PartnerPage from './Pages/PartnerPage/PartnerPage';
 import BookingPage from './Pages/BookingPage/BookingPage';
+import DishesPage from './Pages/DishesPage/DishesPage';
+import AmbiencePage from './Pages/AmbiancePage/AmbiancePage';
+import DeliveryPage from './Pages/DeliveryPage/DeliveryPage';
 
 
 function App() {
@@ -12,10 +15,13 @@ function App() {
         <>
 
             <Header />
-            <WelcomePage/>
+            <WelcomePage />
             <Footer />
             <PartnerPage />
             <BookingPage />
+            <DishesPage />
+            <AmbiencePage />
+            <DeliveryPage />
 
         </>
     )
