db150748f8a9b3f829fe854c6bb39fd9706bcc44
Deepak PG <deepakpg121@gmail.com>
2023-08-28 19:10:51 +0530

    Create route path for pages

---
+++ src/LayoutRoutes/LayoutRoutes.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
+import HomePage from '../pages/HomePage/HomePage'
+import DishesPage from '../pages/DishesPage/DishesPage'
+import BookingPage from '../pages/BookingPage/BookingPage'
+import AboutUsPage from '../pages/AboutUsPage/AboutUsPage'
+import NotFoundPage from '../pages/NotFoundPage/NotFoundPage'
+
+
+function LayoutRoutes() {
+
+    return (
+
+        <Router>
+            <Header />
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/dishes' element={<DishesPage />} />
+                <Route path='/services' element={<BookingPage />} />
+                <Route path='/about-us' element={<AboutUsPage />} />
+                <Route path='/*' element={<NotFoundPage />} />
+            </Routes>
+
+            <Footer />
+        </Router>
+
+    )
+}
+
+export default LayoutRoutes;
