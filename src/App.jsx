f58ff729d7f121ceb9d2591981f971aab9175ef3
Deepak PG <deepakpg121@gmail.com>
2023-08-28 19:13:24 +0530

    Import route

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
 import React from 'react';
-import { useState } from 'react'
-import Header from './Components/Header/Header'
-import WelcomePage from './Pages/WelcomePage/WelcomePage';
-import Footer from './Components/Footer/Footer';
-import PartnerPage from './Pages/PartnerPage/PartnerPage';
-import BookingPage from './Pages/BookingPage/BookingPage';
-import DishesPage from './Pages/DishesPage/DishesPage';
-import AmbiencePage from './Pages/AmbiancePage/AmbiancePage';
-import DeliveryPage from './Pages/DeliveryPage/DeliveryPage';
+//import { useState } from 'react'
+//import Header from './Components/Header/Header'
+//import WelcomePage from './Pages/WelcomePage/WelcomePage';
+//import Footer from './Components/Footer/Footer';
+//import PartnerPage from './Pages/PartnerPage/PartnerPage';
+//import BookingPage from './Pages/BookingPage/BookingPage';
+//import DishesPage from './Pages/DishesPage/DishesPage';
+//import AmbiencePage from './Pages/AmbiancePage/AmbiancePage';
+//import DeliveryPage from './Pages/DeliveryPage/DeliveryPage';
+import LayoutRoutes from './LayoutRoutes/LayoutRoutes';
 
 
 function App() {
     return (
         <>
 
-            <Header />
-            <WelcomePage />
-            <Footer />
-            <PartnerPage />
-            <BookingPage />
-            <DishesPage />
-            <AmbiencePage />
-            <DeliveryPage />
+            <LayoutRoutes />
 
         </>
     )
